d3df6a8f037fa28aec61db37124a4d7980ff9ffe
MrFoxy3010 <fockiret.com@gmail.com>
2021-10-21 19:04:29 +0300

    v1

--- Updater/MainWindow.xaml.cs
+++ Updater/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Updater
 {
@@ -23,6 +11,15 @@ namespace Updater
         public MainWindow()
         {
             InitializeComponent();
+            WorkingWithFiles.eventPercentAndSpeedOnChange += setPercentAndSpeed;
+            LauncherUpdater launcher = new LauncherUpdater();
+            Task.Run(() => launcher.UpdaterLauncher());
+        }
+
+        public void setPercentAndSpeed(double percent)
+        {
+            this.Dispatcher.Invoke(() => percent_text.Content = ((int)percent).ToString() + "%");
+            this.Dispatcher.Invoke(() => progressbar.Value = percent);
         }
     }
 }
